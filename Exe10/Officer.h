970a3f920bbb3ec2b13104d2b803df73e131ec5c
uriehrlich <uriehrlich@gmail.com>
2022-05-31 15:57:25 +0300

    line

--- Exe10/Officer.h
+++ Exe10/Officer.h
@@ -4,7 +4,7 @@
 #include "Soldier.h"
 
 class Officer : public Soldier
-{
+{//aaaawwwwwwwwwwwwwwwwwwwwwwwwwww
 public: //aaaaaaaaaaaaaaaaa
 	Officer (int id, string fn, string ln, int numofop, int sgrade): Soldier(id,fn,ln,numofop), socialGrade(sgrade){} //Constractor
 	Officer(const Officer& other); // copy constractor
